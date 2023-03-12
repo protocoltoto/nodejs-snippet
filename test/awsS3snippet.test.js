9904792965771a122128db26e1a5108fbfb2bac9
Admin <rujji@Rujjis-MacBook-Air.local>
2023-03-12 22:37:38 +0700

    [Update] refactor package.json

--- test/awsS3snippet.test.js
+++ test/awsS3snippet.test.js
@@ -1,29 +1,56 @@
 const s3snippet = require("../src/awsS3upload");
 
 describe("Test Upload file to S3", () => {
-  it("Should Upload file Success when bucket=rujji-test-app ", async () => {
-    let s3bucket = '"rujji-test-app"';
+  // it("Should Upload file Success when bucket=rujji-test-app ", async () => {
+  //   let s3bucket = 'rujji-test-app';
+  //   let filename = "test-folder/output/testfile.txt";
+  //   let content = "TestFile|1|1234erwgasdfa";
+  //   let result = "";
+  //   try {
+  //     result = await s3snippet._uploadToS3(s3bucket, content, filename);
+  //   } catch (error) {
+  //     result = error;
+  //   }
+  //   expect(result).toBeTruthy();
+  // });
+
+  // it("Should Upload file Fail when bucket=notFound ", async () => {
+  //   let s3bucket = 'rujji-test-fail';
+  //   let filename = "test-folder/output/testfile.txt";
+  //   let content = "TestFile|1|1234erwgas123123123dfa";
+  //   let result = "";
+  //   try {
+  //     result = await s3snippet._uploadToS3(s3bucket, content, filename);
+  //   } catch (error) {
+  //     result = error;
+  //   }
+  //   expect(result.code).toBe("InvalidBucketName");
+  // });
+
+  it("Should Download file", async () => {
+    let s3bucket = 'rujji-test-app';
     let filename = "test-folder/output/testfile.txt";
-    let content = "TestFile|1|1234erwgasdfa";
-    let result = "";
     try {
-      result = await s3snippet._uploadToS3(s3bucket, content, filename);
+      result = await s3snippet._downloadS3(s3bucket, filename);
     } catch (error) {
       result = error;
     }
+    console.log("_downloadS3", Buffer.from(result.Body).toString());
     expect(result).toBeTruthy();
   });
-
-  it("Should Upload file Fail when bucket=notFound ", async () => {
-    let s3bucket = '"rujji-test-fail"';
-    let filename = "test-folder/output/testfile.txt";
-    let content = "TestFile|1|1234erwgas123123123dfa";
-    let result = "";
-    try {
-      result = await s3snippet._uploadToS3(s3bucket, content, filename);
-    } catch (error) {
-      result = error;
-    }
-    expect(result.code).toBe('InvalidBucketName');
-  });
+  // it("Should List Object S3", async () => {
+  //   let s3bucket = "rujji-test-app";
+  //   try {
+  //     result = await s3snippet._listObjectS3(s3bucket);
+  //   } catch (error) {
+  //     result = error;
+  //   }
+  //   console.log(
+  //     "_listObjectS3",
+  //     result.Contents.filter((element) => element.Size > 0).map(
+  //       (element) => element.Key
+  //     )
+  //   );
+  //   expect(result).toBeTruthy();
+  // });
 });
